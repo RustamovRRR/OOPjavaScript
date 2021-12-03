a43bfaa96c22fb436674a78673aeebcbb2f0f353
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-03 14:53:52 +0400

    basic objects things

--- objectsBasics/app.js
+++ objectsBasics/app.js
@@ -6,3 +6,37 @@
 
 // object literal syntax, {}
 // dot notation
+
+const person = {
+  name: "john",
+  age: 25,
+  married: true,
+  siblings: ["anna", "piter", "sansa"],
+  greet: function (name) {
+    console.log(`Hi ${name}`);
+  },
+  seyHello(name) {
+    console.log(`Hi ${name}`);
+  },
+};
+
+console.log(person);
+
+// dot notation
+const name = person.name;
+console.log(name);
+
+person.greet("Anna");
+person.seyHello("Vanya");
+
+// adding new property
+person.job = "web developer";
+console.log(person);
+
+// delete
+delete person.siblings;
+console.log(person);
+
+// you can assign the deleted propery it will return boolean value
+const deleted = delete person.age;
+console.log(deleted);
