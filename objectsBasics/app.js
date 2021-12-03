7f7eea094e922137a5f4a95f37ddf8967e561050
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-03 14:28:00 +0400

    setup files for objects basics section

---
+++ objectsBasics/app.js
@@ -0,0 +1,8 @@
+// Objects Basics
+
+// Objects are collections of key(property) value pairs
+// Property values can be strings, numbers, booleans, arrays and functions
+// however if the property value is a function it's called method
+
+// object literal syntax, {}
+// dot notation
