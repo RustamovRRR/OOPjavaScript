bcd1b510e9b6c991f606715804885c9106fed646
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-05 00:32:46 +0400

    constructor function

---
+++ constructorFunctions/app.js
@@ -0,0 +1,35 @@
+// Blue Print
+// Factory Functions and Constructor Functions
+// Constructor Functions
+// new - created new object, points to it, omit return
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = function () {
+    console.log(
+      `My name is ${this.firstName} ${this.lastName}. I love react.js`
+    );
+  };
+}
+
+const anna = new Person("anna", "ivanovna");
+console.log(anna.fullName());
+
+// function createPerson(firstName, lastName) {
+//   return {
+//     firstName: firstName,
+//     lastName: lastName,
+//     fullName: function () {
+//       console.log(this);
+//       console.log(`My name is ${this.firstName} ${this.lastName}`);
+//     },
+//   };
+// }
+
+// const john = createPerson("john", "overmars");
+// john.fullName();
+// const zidan = createPerson("zineddin", "zidan");
+// zidan.fullName();
+// const beckham = createPerson("david", "beckham");
+// beckham.fullName();
