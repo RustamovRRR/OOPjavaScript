2e1ebe0b8bd84af677761173ce48253965da6d8f
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-06 16:55:21 +0400

    working with call method

--- call/app.js
+++ call/app.js
@@ -1,1 +1,30 @@
 // call - runs instantly, arguments - list of items
+
+const anna = {
+  name: "anna",
+  age: 21,
+  greet: function () {
+    console.log(this);
+    console.log(
+      `Hello my name is ${this.name.toUpperCase()}. I am ${this.age} years old`
+    );
+  },
+};
+
+const benjamin = {
+  name: "benjamin",
+  age: 21,
+};
+
+function greet() {
+  console.log(this);
+  console.log(
+    `Hello my name is ${this.name.toUpperCase()}. I am ${this.age} years old`
+  );
+}
+
+anna.greet();
+
+greet.call(benjamin);
+greet.call(anna);
+anna.greet.call(benjamin);
