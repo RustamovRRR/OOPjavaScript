4fa5f8bc5a1061e4c1bfa9f513e967f1c17dd108
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-06 15:35:43 +0400

    setup files for prototype property

---
+++ prototypeProperty/app.js
@@ -0,0 +1,5 @@
+// Prototypal Inheritance Model
+// JavaScript use prototypal inheritance model.
+// That means that every constructor function/class has a prototype property that is shared by every instance of the constructor/class.
+// So any properties and methods or prototype can be accessed by every instance
+// prototype property retuns an object.
